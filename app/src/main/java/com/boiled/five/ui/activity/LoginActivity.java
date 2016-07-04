497b0a1364164aa59a6003a679bae6795e51006f
Ali Iftikhar <ali.iftikhar@incedo.de>
2016-07-04 15:50:53 +0200

    2ND COMMIT

--- app/src/main/java/com/boiled/five/ui/activity/LoginActivity.java
+++ app/src/main/java/com/boiled/five/ui/activity/LoginActivity.java
@@ -1,10 +1,11 @@
 package com.boiled.five.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.boiled.five.R;
 import com.boiled.five.data.DataManager;
@@ -13,6 +14,7 @@ import javax.inject.Inject;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
 
@@ -23,11 +25,26 @@ public class LoginActivity extends BaseActivity {
 
     @Inject DataManager mDataManager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activityComponent().inject(this);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
     }
+    @OnClick(R.id.btn_login)
+    void LoginButton(View view) {
+        if (view.getId() == R.id.btn_login) {
+
+            String username = email.getText().toString();
+            String pass = password.getText().toString();
+
+
+            Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
